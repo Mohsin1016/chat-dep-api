367b467104947c2391c0626ef6b2c171289647f4
Mohsin1016 <muhammadmohsin1016@gmail.com>
2025-01-17 01:19:41 +0500

    slash-issue

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: "https://chat-dep-front.vercel.app/",
+  origin: "https://chat-dep-front.vercel.app",
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
