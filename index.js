5b1d885e35c7cbc405843b6ce76876c81cec1d9e
Mohsin1016@gmail.com <muhammadmohsin1016@gmail.com>
2025-06-29 10:24:32 +0000

    aws code

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: process.env.FRONTEND_URL || "https://chat-dep-front.vercel.app",
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
